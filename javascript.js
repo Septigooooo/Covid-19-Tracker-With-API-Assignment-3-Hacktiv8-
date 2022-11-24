34199f38199622ce09334073c96073b4f23148ec
Septigooooo <20081010177@student.upnjatim.ac.id>
2022-11-24 07:34:09 +0700

    add all

---
+++ javascript.js
@@ -0,0 +1,41 @@
+const active = document.querySelector("#active-cases");
+const newdata = document.querySelector("#new-cases");
+const recovered = document.querySelector("#recovered-cases");
+const totalCases = document.querySelector("#total-cases");
+const totalDeaths = document.querySelector("#total-deaths");
+const totalTest = document.querySelector("#total-test");
+
+const input = document.querySelector("#input");
+const button = document.querySelector("#button");
+
+button.addEventListener("click", async () => {
+  const date = new Date();
+  const today = date.toISOString().slice(0, 10);
+
+  const BASE_URL = `https://covid-193.p.rapidapi.com/history?country=${input.value}&day=${today}`;
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "58976fdb43msha5e707967ccf89dp12988ajsn8e10ebb31e79",
+      "X-RapidAPI-Host": "covid-193.p.rapidapi.com",
+    },
+  };
+
+  const response = await fetch(BASE_URL, options);
+  const data = await response.json();
+
+  if (data.response.length) {
+    const dataResponse = data.response[0];
+
+    console.log(dataResponse);
+    active.innerText = dataResponse.cases.active;
+    newdata.innerText = dataResponse.cases.new ? dataResponse.cases.new : 0;
+    recovered.innerText = dataResponse.cases.recovered;
+    totalCases.innerText = dataResponse.cases.total;
+    totalDeaths.innerText = dataResponse.deaths.total;
+    totalTest.innerText = dataResponse.tests.total;
+  }
+});
+
+
